d3cc95dad1bffeef7b52c4714bafb42f0ff370cb
jbtule <jay+code@tuley.name>
2020-07-07 20:05:44 -0500

    student controller working!

--- ef-mvc/Controllers.fs
+++ ef-mvc/Controllers.fs
@@ -14,19 +14,20 @@ open Microsoft.EntityFrameworkCore
 open ContosoUniversity.Data
 open ContosoUniversity.ViewModels
 open FSharp.Control.Tasks.V2
-open Microsoft.FSharp.Linq.NullableOperators
 open ContosoUniversity.Models
 
 type MVCTask = IActionResult Task
+type UpdateExpr<'T> when 'T: not struct = System.Linq.Expressions.Expression<Func<'T,obj>>
+[<AbstractClass;Sealed>]
+type internal Helper = 
+    static member UpdateExpr ([<ReflectedDefinition>]expr:UpdateExpr<'a>) = expr
+open Helper
 
 type HomeController (logger : ILogger<HomeController>) =
     inherit Controller()
 
-    member this.Index () =
-        this.View()
-
-    member this.Privacy () =
-        this.View()
+    member this.Index () = this.View()
+    member this.Privacy () = this.View()
 
     [<ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)>]
     member this.Error () =
@@ -49,22 +50,24 @@ type StudentsController (context:SchoolContext) =
 
     member this.Details (id:int Nullable) : MVCTask =
         task {
-            if id.HasValue |> not then
-                return upcast this.NotFound()
-            else
+            match Option.ofNullable id with
+            | None -> return upcast this.NotFound()   
+            | Some id' ->
                 let! student = 
                     context
                         .Students
-                        .Include(fun s->s.Enrollments :> IEnumerable<Enrollment>)
+                        .Include(fun s->s.Enrollments :> IEnumerable<_>)
                         .ThenInclude(fun (e:Enrollment)->e.Course)
                         .AsNoTracking()
-                        .FirstOrDefaultAsync(fun m-> m.StudentID =? id)
-                if student |> isNull then
-                    return upcast this.NotFound()
-                else
-                    return upcast this.View(student)
-
+                        .FirstOrDefaultAsync(fun m-> m.StudentID = id')
+                match student |> Option.ofObj with
+                | None -> return upcast this.NotFound()
+                | Some student' ->
+                    return upcast this.View(student')
         }
+
+    member this.Create():IActionResult = upcast this.View()
+
     [<HttpPost;ValidateAntiForgeryToken>]
     member this.Create([<Bind("EnrollmentDate,FirstMidName,LastName")>]student:Student) : MVCTask = 
         task {
@@ -81,65 +84,64 @@ type StudentsController (context:SchoolContext) =
             else
                 return upcast this.View(student)
         }
-(*
+         
+    member this.Edit(id:int Nullable): MVCTask = 
+        task {
+            match id |> Option.ofNullable with
+            | None -> return upcast this.NotFound()
+            | Some id' ->
+                let! student = context.Students.FindAsync(id')
+                match student |> Option.ofObj with
+                | None -> return upcast this.NotFound()
+                | Some student' ->
+                    return upcast this.View(student')
+        }
+
     [<HttpPost;ActionName("Edit");ValidateAntiForgeryToken>]
     member this.EditPost(id:int Nullable) : MVCTask = 
         task {
-            if id.HasValue |> not then
-                return upcast this.NotFound()
-            else
-                let! studentToUpdate = context.Students.FirstOrDefaultAsync(fun s -> s.StudentID =? id)
-                match! this.TryUpdateModelAsync<Student>(studentToUpdate, "", 
-                            fun s -> s.FirstMidName, fun s -> s.LastName, fun s-> s.EnrollmentDate) with
-                | true,_ ->
-                    try
-                        let! _ = context.SaveChangesAsync()
-                        return upcast this.RedirectToAction(nameof Index)
-                    with :? DbUpdateException ->
-                        this.ModelState.AddModelError("", "Unable to save changes. " +
-                            "Try again, and if the problem persists " +
-                            "see your system administrator.");    
-                        return upcast this.View(studentToUpdate)     
-                | false,_ -> 
-                    return upcast this.View(studentToUpdate)    
-
+            match id |> Option.ofNullable with
+            | None -> return upcast this.NotFound()
+            | Some id' ->
+                let! studentToUpdate = context.Students.FirstOrDefaultAsync(fun s -> s.StudentID = id')
+                match studentToUpdate |> Option.ofObj with
+                | None -> return upcast this.NotFound()
+                | Some studentToUpdate' ->
+                    let includeExpr: UpdateExpr<Student> array = 
+                        [|
+                            UpdateExpr(fun s -> upcast s.FirstMidName)
+                            UpdateExpr(fun s -> upcast s.LastName)
+                            UpdateExpr(fun s -> upcast s.EnrollmentDate)
+                        |]
+                    match! this.TryUpdateModelAsync<Student>(studentToUpdate', "", includeExpr) with
+                    | true ->
+                        try
+                            let! _ = context.SaveChangesAsync()
+                            return upcast this.RedirectToAction(nameof Index)
+                        with :? DbUpdateException ->
+                            this.ModelState.AddModelError("", "Unable to save changes. " +
+                                "Try again, and if the problem persists " +
+                                "see your system administrator.");    
+                            return upcast this.View(studentToUpdate')     
+                    | false -> return upcast this.View(studentToUpdate')    
         }
-*)
-
-    member this.Create(id:int, [<Bind("StudentID,EnrollmentDate,FirstMidName,LastName")>]student:Student) : MVCTask = 
-        task {
-            if id <> student.StudentID then
-                return upcast this.NotFound()
-            else if this.ModelState.IsValid then
-                try
-                    context.Update(student) |> ignore
-                    let! _ = context.SaveChangesAsync()
-                    return upcast this.RedirectToAction(nameof Index)
-                with :? DbUpdateException ->
-                    this.ModelState.AddModelError("", "Unable to save changes. " +
-                        "Try again, and if the problem persists " +
-                        "see your system administrator.");    
-                    return upcast this.View(student)     
-            else
-                return upcast this.View(student)
-        }
+        
 
     member this.Delete(id:int Nullable, 
-        [<Optional;DefaultParameterValue(false)>] saveChangesError: bool Nullable) : MVCTask =
+        [<Optional;DefaultParameterValue(false)>] saveChangesError: bool) : MVCTask =
         task {
-            if id.HasValue |> not then
-                return upcast this.NotFound()
-            else
-                let! student = context.Students.FirstOrDefaultAsync(fun s -> s.StudentID =? id)
-                if student |> isNull then
-                    return upcast this.NotFound()
-                else if (saveChangesError |> Option.ofNullable |> Option.defaultValue false) then
-                    this.ViewData.["ErrorMessage"] <-
-                            "Delete failed. Try again, and if the problem persists " +
-                            "see your system administrator."
-                    return upcast this.View(student)
-                else
-                    return upcast this.View(student)
+            match id |> Option.ofNullable with
+            | None -> return upcast this.NotFound()
+            | Some id' ->
+                let! student = context.Students.FirstOrDefaultAsync(fun s -> s.StudentID = id')
+                match student |> Option.ofObj with
+                | None -> return upcast this.NotFound()
+                | Some student' ->
+                    if saveChangesError then
+                        this.ViewData.["ErrorMessage"] <-
+                                "Delete failed. Try again, and if the problem persists " +
+                                "see your system administrator."
+                    return upcast this.View(student')
         }
         
 
@@ -148,14 +150,13 @@ type StudentsController (context:SchoolContext) =
     member this.DeleteConfirmed(id:int) : MVCTask = 
         task {
             let! student = context.Students.FindAsync(id)
-            if student |> isNull then
-                return upcast this.RedirectToAction(nameof this.Index)
-            else 
+            match student |> Option.ofObj with
+            | None -> return upcast this.RedirectToAction(nameof this.Index)
+            | Some student' -> 
                 try
-                    context.Remove(student) |> ignore
+                    context.Remove(student') |> ignore
                     let! _ = context.SaveChangesAsync()
                     return upcast this.RedirectToAction(nameof this.Index)
                 with :? DbUpdateException ->
                     return upcast this.RedirectToAction(nameof this.Delete, {| id = id; saveChangesError = true |})
-
         }
