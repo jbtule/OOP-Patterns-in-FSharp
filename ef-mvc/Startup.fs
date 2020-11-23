c71f40ddce0d28517f7f6daae3a5cdca3116a4fa
jbtule <jay.tuley@ekonbenefits.com>
2020-11-23 17:38:39 -0600

    trying out reducing ignores in start up

--- ef-mvc/Startup.fs
+++ ef-mvc/Startup.fs
@@ -15,7 +15,10 @@ open ContosoUniversity.Data
 open Microsoft.EntityFrameworkCore
 open Microsoft.Extensions.Configuration
 
+
+
 type Startup private () =
+
     new (configuration: IConfiguration) as this =
         Startup() then
         this.Configuration <- configuration
@@ -29,32 +32,33 @@ type Startup private () =
                     this.Configuration.GetConnectionString("DefaultConnection") 
                     |> options.UseSqlite 
                     |> ignore) 
-                |> ignore
+                .AddControllersWithViews().AddRazorRuntimeCompilation() |> ignore
 
-        services.AddControllersWithViews().AddRazorRuntimeCompilation() |> ignore
         services.AddRazorPages() |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
 
-        if (env.IsDevelopment()) then
-            app.UseDeveloperExceptionPage() |> ignore
+        (if (env.IsDevelopment()) then
+            app.UseDeveloperExceptionPage()
         else
-            app.UseExceptionHandler("/Home/Error") |> ignore
+            app.UseExceptionHandler("/Home/Error")
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts() |> ignore
+               .UseHsts()
+        )
+            .UseHttpsRedirection()
+            .UseStaticFiles() 
 
-        app.UseHttpsRedirection() |> ignore
-        app.UseStaticFiles() |> ignore
+            .UseRouting()
 
-        app.UseRouting() |> ignore
+            .UseAuthorization()
 
-        app.UseAuthorization() |> ignore
+            .UseEndpoints(fun endpoints ->
+                endpoints.MapControllerRoute(
+                    name = "default",
+                    pattern = "{controller=Home}/{action=Index}/{id?}") |> ignore
+                endpoints.MapRazorPages() |> ignore)
 
-        app.UseEndpoints(fun endpoints ->
-            endpoints.MapControllerRoute(
-                name = "default",
-                pattern = "{controller=Home}/{action=Index}/{id?}") |> ignore
-            endpoints.MapRazorPages() |> ignore) |> ignore
+            |> ignore
 
     member val Configuration : IConfiguration = null with get, set
