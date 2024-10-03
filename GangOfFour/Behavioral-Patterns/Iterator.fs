b5efbbea3e7319e46b852a670664cfabd3d307b6
Jay Tuley <jay@tuley.name>
2024-10-02 21:17:54 -0500

    Update Iterator to use Object Expression

--- GangOfFour/Behavioral-Patterns/Iterator.fs
+++ GangOfFour/Behavioral-Patterns/Iterator.fs
@@ -22,27 +22,28 @@ type MyList<'T> (?optSize:int) =
     member _.Count with get () = store.Count
     member _.Item with get index = store.[index]
     interface seq<'T> with
-        member _.GetEnumerator () : IEnumerator<'T> =
-            upcast new MyEnumerator<'T>(store)
+        member _.GetEnumerator () =
+            // This object expression implements the IEnumerator<'T> interface
+            // without making a new named type
+            let mutable current = 0;
+            { 
+                new IEnumerator<'T> with
+                   member this.Current with get (): 'T = 
+                        if store.Count > current then
+                            store.[current]
+                        else
+                            failwith "Enumerator Out Of Bounds"
+                    member this.Current with get(): obj = 
+                        this.Current
+                    member _.Dispose(): unit = ()
+                    member _.MoveNext(): bool = 
+                        current <- current + 1
+                        store.Count > current
+                    member this.Reset(): unit = 
+                        current <- 0     
+            }
         member this.GetEnumerator(): System.Collections.IEnumerator = 
             upcast (this :> IEnumerable<'T>).GetEnumerator()
-
-and MyEnumerator<'T> (list: ResizeArray<'T>) =
-    let mutable current = 0;
-    interface IEnumerator<'T> with
-        member this.Current with get (): 'T = 
-            if list.Count > current then
-                list.[current]
-            else
-                failwith "Enumerator Out Of Bounds"
-        member this.Current with get(): obj = 
-            upcast (this :> IEnumerator<'T>).Current
-        member _.Dispose(): unit = ()
-        member _.MoveNext(): bool = 
-            current <- current + 1
-            list.Count > current
-        member this.Reset(): unit = 
-            current <- 0           
 
 ///Example
 
